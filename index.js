b7937f4f5781de910c5afb5a00b92a3f6c0b74b6
David Everett <davideverett1989@gmail.com>
2020-07-05 21:24:45 -0500

    wip

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const sgMail = require('@sendgrid/mail');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -30,21 +31,23 @@ app.post('/sms', jsonParser, (req, res) => {
 });
 
 app.post('/email', jsonParser, (req, res) => {
-  const accountSid = process.env.ACCOUNT_SID;
-  const authToken = process.env.AUTH_TOKEN;
-  const fromEmail = process.env.EMAIL;
-  const toEmail = req.body.sendEmail;
-  const message = req.body.message;
-  const client = require('twilio')(accountSid, authToken);
-  client.messages
-  .create({
-     body: message,
-     from: fromEmail,
-     to: toEmail
-   })
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const toEmail = req.body.toEmail;
+  const subject = req.body.subject;
+  const text = req.body.text;
+  const msg = {
+    to: toEmail,
+    from: 'davideverett1989@gmail.com',
+    subject: subject,
+    text: text,
+    // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail
+  .send(msg)
   .then(() => res.sendStatus(200))
-  .catch(() => res.sendStatus(500));
+  .catch(() => res.sendStatus(500))
 });
+
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
