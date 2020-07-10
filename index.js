2e3a2320879206fd5bc86eb33be25cb0ed42f1a8
David Everett <davideverett1989@gmail.com>
2020-07-10 14:07:50 -0500

    wip

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const cron = require("node-cron");
+const fs = require("fs");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+const axios = require('axios');
+const moment = require('moment');
+
+const baseUrl = 'https://socially-distant-birthday.firebaseio.com';
+
 const jsonParser = bodyParser.json();
+const today = moment().format('YYYY-MM-DD');
 
 
 app.use((req, res, next) => {
@@ -66,7 +74,61 @@ app.post('/send', jsonParser, (req, res) => {
   res.sendStatus(200);
 });
 
+cron.schedule("* * * * * *", () => {
+  getUserEmailsWithCurrentBirthday(today)
+    .then((emails) => {
+      console.log(emails);
+    })
+    .catch((err) => console.error('There was an issue with getting today\'s birthday emails:', err));
+});
 
+const getBirthdays = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/birthdays.json`)
+    .then((response) => {
+      const birthdayObject = response.data;
+      const birthdays = [];
+      if (birthdayObject !== null) {
+        Object.keys(birthdayObject).forEach((birthdayId) => {
+          birthdayObject[birthdayId].id = birthdayId;
+          birthdays.push(birthdayObject[birthdayId]);
+        });
+      }
+      resolve(birthdays);
+    })
+    .catch((err) => reject(err));
+});
+
+const getUsers = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/users.json`)
+    .then((response) => {
+      const userObject = response.data;
+      const users = [];
+      if (userObject !== null) {
+        Object.keys(userObject).forEach((userId) => {
+          userObject[userId].id = userId;
+          users.push(userObject[userId]);
+        });
+      }
+
+      resolve(users);
+    })
+    .catch((err) => reject(err));
+});
+
+const getUserEmailsWithCurrentBirthday = (date) => new Promise((resolve, reject) => {
+  getBirthdays().then((birthdays) => {
+    getUsers().then((users) => {
+      const finalUserEmails = [];
+      const todaysBirthdays = birthdays.filter((x) => x.date === date);
+      todaysBirthdays.forEach((birthday) => {
+        const guestOfHonor = users.find((x) => x.id === birthday.guestOfHonorId);
+        finalUserEmails.push(guestOfHonor.email);
+      });
+      resolve(finalUserEmails);
+    });
+  })
+    .catch((err) => reject(err));
+});
 
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
