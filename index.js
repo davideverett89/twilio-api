c5aa33c91aeac90f50c56edeeebed433ae38e468
David Everett <davideverett1989@gmail.com>
2020-07-09 19:35:32 -0500

    whatever

--- index.js
+++ index.js
@@ -2,22 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
-const path = require('path');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-// const jsonParser = bodyParser.json();
+const jsonParser = bodyParser.json();
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.render('contact');
-})
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -25,24 +14,24 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.post('/sms', jsonParser, (req, res) => {
-//   const accountSid = process.env.ACCOUNT_SID;
-//   const authToken = process.env.AUTH_TOKEN;
-//   const fromNumber = process.env.PHONE_NUM;
-//   const toNumber = req.body.sendNumber;
-//   const message = req.body.message;
-//   const client = require('twilio')(accountSid, authToken);
-//   client.messages
-//   .create({
-//      body: message,
-//      from: fromNumber,
-//      to: toNumber
-//    })
-//   .then(() => res.sendStatus(200))
-//   .catch(() => res.sendStatus(500));
-// });
+app.post('/sms', jsonParser, (req, res) => {
+  const accountSid = process.env.ACCOUNT_SID;
+  const authToken = process.env.AUTH_TOKEN;
+  const fromNumber = process.env.PHONE_NUM;
+  const toNumber = req.body.sendNumber;
+  const message = req.body.message;
+  const client = require('twilio')(accountSid, authToken);
+  client.messages
+  .create({
+     body: message,
+     from: fromNumber,
+     to: toNumber
+   })
+  .then(() => res.sendStatus(200))
+  .catch(() => res.sendStatus(500));
+});
 
-app.post('/send', (req, res) => {
+app.post('/send', jsonParser, (req, res) => {
   console.log(req.body);
 });
 
